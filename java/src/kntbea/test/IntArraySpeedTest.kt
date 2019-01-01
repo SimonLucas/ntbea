a41f71ed02a6f4df4644c6c4dcad7a333aff56f7
SImon Lucas <simonmarklucas@gmail.com>
2019-01-01 23:14:08 +0000

    Ready for Kotlin version

---
+++ java/src/kntbea/test/IntArraySpeedTest.kt
@@ -0,0 +1,12 @@
+package kntbea.test
+
+import utilities.ElapsedTimer
+
+fun main(args: Array<String>) {
+    val t = ElapsedTimer();
+    val n =10;
+    for (i in 0 until 10) {
+        println(i)
+    }
+
+}
